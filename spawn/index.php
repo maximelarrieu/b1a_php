f358cd1f04667929a5d5b3647917bf06429f7cc5
Maxime Larrieu <43401890+maximelarrieu@users.noreply.github.com>
2019-05-22 22:24:27 +0200

    index correction

--- spawn/index.php
+++ spawn/index.php
@@ -18,23 +18,7 @@
 	<div id="spawned">
 	<input type="submit" id="btspawn" name="randspawn" value="SPAWN">
 	<?php
-		if (isset($_POST['randspawn'])) {
-				$query = $pdo->prepare('SELECT name FROM spawn ORDER BY RAND() LIMIT 1;');
-		$query->execute();
-		$spawns = $query->fetchAll();
-		foreach ($spawns as $spawn) {
-			echo '<h3>Spawn : ' . $spawn['name'] . "\n</h3>";
-		}
-		if ($spawn['name'] == "Tilted Towers") {
-			echo '<img class="spawn" src="ressources/tiltedtowers.png" />';
-		}
-		elseif ($spawn['name'] == "Fatal Fields") {
-			echo '<img class="spawn" src="ressources/fatalfields.png" />';
-		}
-		else {
-			echo '<img class="spawn" src="ressources/tomatotown.png" />';
-		}
-			}
+		include 'spawn.php';
 	?>
 	</div>
 	<div id="admin">
